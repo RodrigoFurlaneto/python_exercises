614618b77e26e4e332d4e65aafb7dbca241eb901
Rodrigo Furlaneto <rodrigo.ffurlaneto@gmail.com>
2022-11-27 19:55:44 -0300

    Exercise 006

--- 006.py
+++ 006.py
@@ -2,6 +2,6 @@ number = int(input('Enter a number: '))
 
 double = number * 2
 triple = number * 3
-square = number ** 1/2
+square = number ** (1/2)
 
-print("The number {}'s double is {}, its triple is {} and its square root is {}".format(number, double, triple, square))
+print("The number {}'s double is {}\nIts triple is {}\nIts square root is {:.2f}".format(number, double, triple, square))
