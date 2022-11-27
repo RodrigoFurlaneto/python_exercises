4eccf2d1aa5e7ec97992e1ff7320c2a3f8a22f6a
Rodrigo Furlaneto <rodrigo.ffurlaneto@gmail.com>
2022-11-27 20:05:23 -0300

    Exercise 008

--- 008.py
+++ 008.py
@@ -3,4 +3,4 @@ meters = float(input('Enter the value in meters: '))
 centimeters = meters * 100
 millimeters = meters * 1000
 
-print('{} meters is equal {} centimeters and {} millimeters'.format(meters, centimeters, millimeters))
+print('{} meters is equal to {:.0f} centimeters and {:.0f} millimeters'.format(meters, centimeters, millimeters))
