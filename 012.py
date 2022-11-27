0981afdb48142acd833b4ddb3aa148a41f953780
Rodrigo Furlaneto <rodrigo.ffurlaneto@gmail.com>
2022-11-27 19:32:31 -0300

    Exercise 012

---
+++ 012.py
@@ -0,0 +1,7 @@
+# This application will return a new product price (5% off)
+
+old_price = float(input('Enter the product price: '))
+
+five_percent_off = old_price * 0.95
+
+print('The new product price is ${:.2f}'.format(five_percent_off))
