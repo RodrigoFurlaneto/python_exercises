820a115bdd0adea9210faa2ebfc056641141e280
Rodrigo Furlaneto <rodrigo.ffurlaneto@gmail.com>
2022-11-27 19:41:05 -0300

    Exercise 013

---
+++ 013.py
@@ -0,0 +1,7 @@
+# This application will read a salary and give a 15% boost
+
+salary = float(input('Enter the salary: '))
+
+new_salary = salary * 1.15
+
+print('Your new salary with a 15% increase is ${:.2f}'.format(new_salary))
