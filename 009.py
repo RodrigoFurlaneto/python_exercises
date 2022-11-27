67cab3092e5fe2b37ee9d6ec598c47db0004c119
Rodrigo Furlaneto <rodrigo.ffurlaneto@gmail.com>
2022-11-27 18:44:36 -0300

    Exercise 009

---
+++ 009.py
@@ -0,0 +1,12 @@
+number = int(input('Enter a number: '))
+
+print('1 x {} = {}'.format(number, number * 1))
+print('2 x {} = {}'.format(number, number * 2))
+print('3 x {} = {}'.format(number, number * 3))
+print('4 x {} = {}'.format(number, number * 4))
+print('5 x {} = {}'.format(number, number * 5))
+print('6 x {} = {}'.format(number, number * 6))
+print('7 x {} = {}'.format(number, number * 7))
+print('8 x {} = {}'.format(number, number * 8))
+print('9 x {} = {}'.format(number, number * 9))
+print('10 x {} = {}'.format(number, number * 10))
