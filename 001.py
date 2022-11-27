f67771d422ec199f4ca4a4694d72acde09af7211
Rodrigo Furlaneto <rodrigo.ffurlaneto@gmail.com>
2022-11-27 14:14:16 -0300

    Exercise 001

---
+++ 001.py
@@ -0,0 +1,6 @@
+number_one = input('Enter a number: ')
+number_two = input('Enter another number: ')
+
+print('The sum is: {}'.format(number_one + number_two))
+print('Oops, it is wrong...')
+print('Actually the sum is: {}'.format(int(number_one) + int(number_two)))
