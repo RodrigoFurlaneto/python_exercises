80650f230ac146b5be8211039dff362b548c16c0
Rodrigo Furlaneto <rodrigo.ffurlaneto@gmail.com>
2022-11-27 14:30:40 -0300

    Exercise 003

---
+++ 003.py
@@ -0,0 +1,5 @@
+number_one = int(input('Enter a number: '))
+number_two = int(input('Enter another number: '))
+sum = number_one + number_two
+
+print('The sum between {} and {} is {}'.format(number_one, number_two, sum))
