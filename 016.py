df4c48248b13e4f1fc2e0d24a38d18fb894f0c89
Rodrigo Furlaneto <rodrigo.ffurlaneto@gmail.com>
2022-11-28 21:30:48 -0300

    Exercise 016

---
+++ 016.py
@@ -0,0 +1,5 @@
+# Testing the trunc function
+from math import trunc
+
+random_number = float(input('Enter a number: '))
+print('The whole number of {} is {}'.format(random_number, trunc(random_number)))
